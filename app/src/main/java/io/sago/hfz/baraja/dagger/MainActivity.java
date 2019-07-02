d3b3da8636907c9381b02ddd255aeb5080373fd2
harris febryantony z <harris.febryantony@dana.id>
2019-07-02 16:50:14 +0700

    initial commit

---
+++ app/src/main/java/io/sago/hfz/baraja/dagger/MainActivity.java
@@ -0,0 +1,16 @@
+package io.sago.hfz.baraja.dagger;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    Retrofit
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
